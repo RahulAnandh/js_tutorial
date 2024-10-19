bca95773c7d84519c84322be2fe6af65afdc58c7
RahulAnandh <rahulanandh80@gmail.com>
2024-10-19 17:48:44 +0530

    object proparties

--- src/components/objects.js
+++ src/components/objects.js
@@ -5,11 +5,22 @@ const ObjectIndex = () => {
     tire: 4,
     seat: 6,
     airbag: true,
+    phone: {
+      phone1: "8921970340",
+      phone2: "8921970341",
+    },
     showProparties: () => {
       return <div>hai</div>;
     },
   };
   car_object.seat = 4;
+  console.log(car_object);
+  delete car_object.airbag;
+  console.log(car_object);
+  delete car_object["tire"];
+  console.log(car_object);
+  console.log(car_object["phone"]["phone1"]);
+
   return (
     <div>
       {car_object.name}:{car_object.seat} seats with {car_object.color} color
